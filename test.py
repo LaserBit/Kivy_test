acc6b47b9df7f809a54e62692c4039a813f9cd5e
LaserBit <laserbit@fake.bit>
2019-03-25 18:29:13 +0900

    ボタンを押すたびに文字の表示非表示が入れ替わる

---
+++ test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+
+
+class TextWidget(Widget):
+    text = StringProperty()    # プロパティの追加
+
+    def __init__(self, **kwargs):
+        super(TextWidget, self).__init__(**kwargs)
+        self.text = ''
+
+    def clicked(self):  # ボタンをクリック時
+        if self.text == '':
+            self.text = 'Hello World'
+        else:
+            self.text = ''
+
+
+class TestApp(App):
+    def __init__(self, **kwargs):
+        super(TestApp, self).__init__(**kwargs)
+        self.title = 'TESTING'  # ウィンドウの名前を変更
+
+    def build(self):
+        return TextWidget()
+
+
+if __name__ == '__main__':
+    TestApp().run()
