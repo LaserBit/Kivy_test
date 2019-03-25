410e9c28511c6ff859dc4893ebfae1ff8aad25a6
LaserBit <laserbit@fake.bit>
2019-03-25 19:31:26 +0900

    ボタンを押すたびに文字の表示非表示が入れ替わる

--- buttonClicked.py
+++ buttonClicked.py
@@ -1,32 +1,21 @@
 # -*- coding: utf-8 -*-
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty, NumericProperty
-from kivy.clock import Clock
-from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.properties import StringProperty
 
 
 class TextWidget(Widget):
     text = StringProperty()    # プロパティの追加
-    time = NumericProperty()
 
     def __init__(self, **kwargs):
         super(TextWidget, self).__init__(**kwargs)
-        self.text = ""
-        self.time = 0
-        self.event = Clock.schedule_interval(self.update_time, 1.0)
-        pass
-
-    def update_time(self, *args):
-        self.time += 1
+        self.text = ''
 
     def clicked(self):  # ボタンをクリック時
-        if self.time == 10:
-            self.event.cancel()
-            self.text = "おめでとう!!"
+        if self.text == '':
+            self.text = 'Hello World'
         else:
-            self.text = "ブブー(笑)"
-            self.time = 0
+            self.text = ''
 
 
 class TestApp(App):
@@ -39,5 +28,4 @@ class TestApp(App):
 
 
 if __name__ == '__main__':
-    LabelBase.register(DEFAULT_FONT, "ipaexg.ttf")
     TestApp().run()
